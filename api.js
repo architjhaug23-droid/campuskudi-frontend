ec5c6155ed7f69e711f5aca36de5d49111eef2da
architjhaug23-droid <archit.jha.ug23@nsut.ac.in>
2026-06-23 21:00:01 +0530

    api.js

--- api.js
+++ api.js
@@ -1,46 +1,83 @@
 // js/api.js
 
-const API_URL =
-"http://campuskudi.com/api";
+const API_URL = "http://api.campuskudi.com/api";
 
-function getToken(){
-
- return localStorage.getItem(
- 'token'
- );
+function getToken() {
+    return localStorage.getItem("token");
 }
 
-async function getData(
- endpoint
-){
+// GET REQUEST
+async function getData(endpoint) {
 
- const response =
- await fetch(
- `${API_URL}${endpoint}`
- );
+    const response = await fetch(
+        `${API_URL}${endpoint}`,
+        {
+            headers: {
+                Authorization: getToken()
+                    ? `Bearer ${getToken()}`
+                    : ""
+            }
+        }
+    );
 
- return response.json();
+    return response.json();
 }
 
-async function postData(
- endpoint,
- data
-){
+// POST REQUEST
+async function postData(endpoint, data) {
 
- const response =
- await fetch(
- `${API_URL}${endpoint}`,
- {
-   method:'POST',
+    const response = await fetch(
+        `${API_URL}${endpoint}`,
+        {
+            method: "POST",
 
-   headers:{
-      'Content-Type':
-      'application/json'
-   },
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: getToken()
+                    ? `Bearer ${getToken()}`
+                    : ""
+            },
 
-   body:
-   JSON.stringify(data)
- });
+            body: JSON.stringify(data)
+        }
+    );
 
- return response.json();
+    return response.json();
+}
+
+// PUT REQUEST
+async function putData(endpoint, data) {
+
+    const response = await fetch(
+        `${API_URL}${endpoint}`,
+        {
+            method: "PUT",
+
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${getToken()}`
+            },
+
+            body: JSON.stringify(data)
+        }
+    );
+
+    return response.json();
+}
+
+// DELETE REQUEST
+async function deleteData(endpoint) {
+
+    const response = await fetch(
+        `${API_URL}${endpoint}`,
+        {
+            method: "DELETE",
+
+            headers: {
+                Authorization: `Bearer ${getToken()}`
+            }
+        }
+    );
+
+    return response.json();
 }
